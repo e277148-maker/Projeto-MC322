bc9356c7afa60e3c0bbe43c21cf5c76f40d667ef
Enzo Tonus <e277148@dac.unicamp.br>
2026-03-31 14:30:13 -0300

    Criação da função do turno do inimigo e do turno do heroi

--- src/batalhas/Batalha.java
+++ src/batalhas/Batalha.java
@@ -28,8 +28,121 @@ public class Batalha {
 
         // Loop principal com alternancia de turnos, até que alguem morra
         while (heroi.getVida() > 0 && inimigo.getVida() > 0) {
+            //Turno do heroi
+            turnoDoHeroi(heroi, inimigo, baralho, scanner);
+            // Fim do turno do heroi
 
-            // reseta escudo e energia do heroi no inicio do turno
+            // Turno do inimigo
+            turnoDoInimigo(inimigo, heroi, defesaInimigo);
+            // Fim do turno do inimigo
+
+            notificar(eventos.get(1));
+
+            if (!heroi.estarVivo()) {  // Se o heroi morrer o jogo acaba
+                System.out.println("\nQue pena... Você perdeu o combate");
+                break ponto_controle;
+            }
+
+        } // Fim do while principal
+
+    }
+
+    private void imprimirMenu(Baralho baralho, Heroi heroi, Inimigo inimigo){
+
+        System.out.println("===============================================================================");
+        System.out.println("Cartas no Baralho: " + baralho.getPilhaDeCompras().size() + " | Cartas no Descarte: " + baralho.getPilhaDeDescarte().size());
+        System.out.println("Próxima carta a ser comprada: [" + baralho.verTopoBaralho() + "]");
+        System.out.println("===============================================================================");
+        System.out.println("");
+        System.out.printf("%s (%d/50 de vida) (%d de escudo) (%d/6 de energia)\n", heroi.getNome(), heroi.getVida(), heroi.getEscudo(), heroi.getEnergia());
+
+        if (!heroi.getEfeitos().isEmpty()) {
+            System.out.print("Efeitos ativos: ");
+            for (Efeito e : heroi.getEfeitos()) {
+                System.out.print("[" + e.getString() + "] ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("vs");
+        System.out.printf("%s (%d/30 de vida) (%d de escudo)\n", inimigo.getNome(), inimigo.getVida(), inimigo.getEscudo());
+
+        if (!inimigo.getEfeitos().isEmpty()) {
+            System.out.print("Efeitos ativos: ");
+            for (Efeito e : inimigo.getEfeitos()) {
+                System.out.print("[" + e.getString() + "] ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("");
+        System.out.println("===============================================================================");
+        System.out.println("");
+        System.out.println("Você pode: ");
+        System.out.println("");
+
+        for (int j = 0; j < baralho.getMao().size(); j++){
+            System.out.printf("%d - usar %s - %d de energia\n", j, baralho.getMao().get(j).getNome(), baralho.getMao().get(j).getCusto());
+        }
+
+        System.out.println("");
+        int opcaoEncerrar = baralho.getMao().size();
+        
+        System.out.printf("%d - Encerrar turno \n", opcaoEncerrar);
+        System.out.printf("%d - Descartar uma carta manualmente (0 energia)\n", opcaoEncerrar + 1);
+        System.out.printf("%d - Comprar uma carta extra (1 energia)\n", opcaoEncerrar + 2);
+        
+        System.out.println("");
+        System.out.println("Escolha: ");
+        System.out.println("");
+    }
+
+
+
+
+    private void turnoDoInimigo(Inimigo inimigo, Heroi heroi, int defesaInimigo){
+        if (inimigo.getVida() > 0){
+
+                Random random = new Random();
+                int acaoInimigo = random.nextInt(5); // Alterado para 5 para incluir a cura!
+
+                switch (acaoInimigo) {
+                    case 0: // Ataca
+                        inimigo.atacar(heroi);
+                        System.out.println("Inimigo atacou\n");
+                        break;
+                    case 1: // Ganha escudo
+                        inimigo.ganharEscudo(defesaInimigo);
+                        System.out.printf("%s ganhou escudo\n", inimigo.getNome());
+                        break;
+                    case 2: // Ataca e ganha escudo
+                        inimigo.atacar(heroi);
+                        inimigo.ganharEscudo(defesaInimigo);
+                        System.out.printf("%s atacou e ganhou escudo\n", inimigo.getNome());
+                        break;
+                    case 3: // Usar veneno
+                        System.out.printf("%s envenenou heroi\n", inimigo.getNome());
+                        inimigo.envenenar(heroi);
+                        break;
+                    case 4: // Usar cura
+                        System.out.printf("%s usou cura\n", inimigo.getNome());
+                        inimigo.curar(inimigo);
+                        break;
+                }
+
+                System.out.println("-----------------------------------------");
+                try {
+                    // Pede para o Java pausar a execução por 2500 milissegundos (2.5 segundos)
+                    Thread.sleep(2500); 
+                } catch (InterruptedException e) {
+                    System.out.println("Aviso: A pausa do turno foi interrompida.");
+                }
+                
+            }
+    }
+
+    private void turnoDoHeroi(Heroi heroi, Inimigo inimigo, Baralho baralho, Scanner scanner){
+        // reseta escudo e energia do heroi no inicio do turno
             heroi.setTurno(true);
             heroi.setEscudo(0);
             heroi.setEnergia(6);
@@ -88,107 +201,6 @@ public class Batalha {
                 }
 
             } // Fim do turno do heroi
-
-            // Turno do inimigo
-            if (inimigo.getVida() > 0){
-
-                Random random = new Random();
-                int acaoInimigo = random.nextInt(5); // Alterado para 5 para incluir a cura!
-
-                switch (acaoInimigo) {
-                    case 0: // Ataca
-                        inimigo.atacar(heroi);
-                        System.out.println("Inimigo atacou\n");
-                        break;
-                    case 1: // Ganha escudo
-                        inimigo.ganharEscudo(defesaInimigo);
-                        System.out.printf("%s ganhou escudo\n", inimigo.getNome());
-                        break;
-                    case 2: // Ataca e ganha escudo
-                        inimigo.atacar(heroi);
-                        inimigo.ganharEscudo(defesaInimigo);
-                        System.out.printf("%s atacou e ganhou escudo\n", inimigo.getNome());
-                        break;
-                    case 3: // Usar veneno
-                        System.out.printf("%s envenenou heroi\n", inimigo.getNome());
-                        inimigo.envenenar(heroi);
-                        break;
-                    case 4: // Usar cura
-                        System.out.printf("%s usou cura\n", inimigo.getNome());
-                        inimigo.curar(inimigo);
-                        break;
-                }
-
-                System.out.println("-----------------------------------------");
-                try {
-                    // Pede para o Java pausar a execução por 2500 milissegundos (2.5 segundos)
-                    Thread.sleep(2500); 
-                } catch (InterruptedException e) {
-                    System.out.println("Aviso: A pausa do turno foi interrompida.");
-                }
-                
-            }
-            // Fim do turno do inimigo
-
-            notificar(eventos.get(1));
-
-            if (!heroi.estarVivo()) {  // Se o heroi morrer o jogo acaba
-                System.out.println("\nQue pena... Você perdeu o combate");
-                break ponto_controle;
-            }
-
-        } // Fim do while principal
-
-    }
-
-    public void imprimirMenu(Baralho baralho, Heroi heroi, Inimigo inimigo){
-
-        System.out.println("===============================================================================");
-        System.out.println("Cartas no Baralho: " + baralho.getPilhaDeCompras().size() + " | Cartas no Descarte: " + baralho.getPilhaDeDescarte().size());
-        System.out.println("Próxima carta a ser comprada: [" + baralho.verTopoBaralho() + "]");
-        System.out.println("===============================================================================");
-        System.out.println("");
-        System.out.printf("%s (%d/50 de vida) (%d de escudo) (%d/6 de energia)\n", heroi.getNome(), heroi.getVida(), heroi.getEscudo(), heroi.getEnergia());
-
-        if (!heroi.getEfeitos().isEmpty()) {
-            System.out.print("Efeitos ativos: ");
-            for (Efeito e : heroi.getEfeitos()) {
-                System.out.print("[" + e.getString() + "] ");
-            }
-            System.out.println();
-        }
-
-        System.out.println("vs");
-        System.out.printf("%s (%d/30 de vida) (%d de escudo)\n", inimigo.getNome(), inimigo.getVida(), inimigo.getEscudo());
-
-        if (!inimigo.getEfeitos().isEmpty()) {
-            System.out.print("Efeitos ativos: ");
-            for (Efeito e : inimigo.getEfeitos()) {
-                System.out.print("[" + e.getString() + "] ");
-            }
-            System.out.println();
-        }
-
-        System.out.println("");
-        System.out.println("===============================================================================");
-        System.out.println("");
-        System.out.println("Você pode: ");
-        System.out.println("");
-
-        for (int j = 0; j < baralho.getMao().size(); j++){
-            System.out.printf("%d - usar %s - %d de energia\n", j, baralho.getMao().get(j).getNome(), baralho.getMao().get(j).getCusto());
-        }
-
-        System.out.println("");
-        int opcaoEncerrar = baralho.getMao().size();
-        
-        System.out.printf("%d - Encerrar turno \n", opcaoEncerrar);
-        System.out.printf("%d - Descartar uma carta manualmente (0 energia)\n", opcaoEncerrar + 1);
-        System.out.printf("%d - Comprar uma carta extra (1 energia)\n", opcaoEncerrar + 2);
-        
-        System.out.println("");
-        System.out.println("Escolha: ");
-        System.out.println("");
     }
 
     // Metodos do publisher
